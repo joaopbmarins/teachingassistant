ded881015b7f9712ce4a6f39f4c3b5f68ed0119e
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2025-11-04 17:45:44 -0300

    feat(aluno): add field github

--- ta-gui/src/app/app.ts
+++ ta-gui/src/app/app.ts
@@ -8,11 +8,12 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('Cadastro de Alunos');
-  aluno: Aluno = { nome: '', cpf: '', email: '' };
+  aluno: Aluno = { nome: '', cpf: '', email: '', github: ''};
 }
 
 export class Aluno {
   nome: string = '';
   cpf: string = '';
   email: string = '';
+  github: string= '';
 }
